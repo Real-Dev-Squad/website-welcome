32fd38ff264c84d887fb8331cbd8958997e2e120
ashish <ashishmangla1991@gmail.com>
2020-08-08 15:31:02 +0530

    add style to faq page

---
+++ js/faq.js
@@ -0,0 +1,14 @@
+const faqButtons = document.querySelectorAll('.faq__btn');
+const faqs = document.querySelectorAll('.faq');
+faqButtons.forEach( function(btn){
+    btn.addEventListener('click', function (e) {
+        const faq = e.target.closest('.faq')
+        faqs.forEach(function (item) {
+            if (item !== faq) {
+                item.classList.remove('faq--show');
+            }
+        });
+        faq.classList.toggle('faq--show');
+    })
+   
+})
