6422b6f28a9ba2831e684f5de7b169fef96aec05
rucha.patil18@vit.edu <rucha.patil18@vit.edu>
2020-10-13 14:43:24 +0530

    Changes as per suggested in review done

--- js/login.js
+++ js/login.js
@@ -1,15 +1,15 @@
 function setNameIfFound(name){
-    const userLogin = document.querySelector("#login-btn")
+    const userLogin = document.querySelector("#login-btn");
     if(name){
-        userLogin.innerHTML = `Hello ${name}!`
-    }
-}
+        userLogin.innerHTML = `Hello ${name}!`;
+    };
+};
 
-let url = "https://staging-api.realdevsquad.com/users/self"
+let url = "https://staging-api.realdevsquad.com/users/self";
 
 fetch(url, {
     "credentials": "include"
 })
 .then(res => res.json())
 .then(res => setNameIfFound(res.first_name))
-.catch(err => console.log(err))
+.catch(err => console.log(err));
