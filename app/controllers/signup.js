731a9ed384e83f8115bc9a8f8f794d9873da82de
sagarokcredit <sagar@okcredit.in>
2021-01-15 10:47:22 +0530

    Ref(signup.js): Double Quotes replaced

--- app/controllers/signup.js
+++ app/controllers/signup.js
@@ -1,25 +1,25 @@
-import Controller from "@ember/controller";
-import { tracked } from "@glimmer/tracking";
-import { action, set } from "@ember/object";
-import HistoryLocation from "@ember/routing/history-location";
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { action, set } from '@ember/object';
+import HistoryLocation from '@ember/routing/history-location';
 
-const BASE_URL = "https://staging-api.realdevsquad.com";
+const BASE_URL = 'https://staging-api.realdevsquad.com';
 
 export default class SignupController extends Controller {
   @tracked isSubmitDisabled = true;
-  @tracked title = "Account Details";
+  @tracked title = 'Account Details';
   @tracked formData = {
-    first_name: "",
-    last_name: "",
-    username: "",
-    email: "",
-    phone_number: "",
-    yoe: "",
-    company_name: "",
-    designation: "",
-    github_id: "",
-    linkedin_id: "",
-    twitter_id: "",
+    first_name: '',
+    last_name: '',
+    username: '',
+    email: '',
+    phone_number: '',
+    yoe: '',
+    company_name: '',
+    designation: '',
+    github_id: '',
+    linkedin_id: '',
+    twitter_id: '',
   };
 
   @tracked formErrors = {
@@ -38,101 +38,101 @@ export default class SignupController extends Controller {
 
   @tracked fields = [
     {
-      id: "first_name",
-      label: "First Name",
-      type: "text",
-      errorMessage: "First name is required",
+      id: 'first_name',
+      label: 'First Name',
+      type: 'text',
+      errorMessage: 'First name is required',
       required: true,
       showError: false,
     },
     {
-      id: "last_name",
-      label: "Last Name",
-      type: "text",
-      errorMessage: "Last name is required",
+      id: 'last_name',
+      label: 'Last Name',
+      type: 'text',
+      errorMessage: 'Last name is required',
       required: true,
       showError: false,
     },
     {
-      id: "username",
-      label: "Choose a username",
-      type: "text",
-      errorMessage: "Username is required",
+      id: 'username',
+      label: 'Choose a username',
+      type: 'text',
+      errorMessage: 'Username is required',
       required: true,
       showError: false,
     },
     {
-      id: "email",
-      label: "Email",
-      type: "email",
-      errorMessage: "Valid Email is required",
+      id: 'email',
+      label: 'Email',
+      type: 'email',
+      errorMessage: 'Valid Email is required',
       required: true,
       showError: false,
       validator: this.emailValidator,
     },
     {
-      id: "phone_number",
-      label: "Phone Number",
-      type: "string",
-      value: "+91-",
-      errorMessage: "Enter a valid phone number",
+      id: 'phone_number',
+      label: 'Phone Number',
+      type: 'string',
+      value: '+91-',
+      errorMessage: 'Enter a valid phone number',
       required: false,
       showError: false,
       validator: this.phone_numberValidator,
     },
     {
-      id: "yoe",
-      label: "yoe",
-      type: "number",
-      errorMessage: "Number of years of experience is required",
+      id: 'yoe',
+      label: 'yoe',
+      type: 'number',
+      errorMessage: 'Number of years of experience is required',
       required: true,
       showError: false,
     },
     {
-      id: "company_name",
-      label: "Company Name / College Name ",
-      type: "text",
-      errorMessage: "",
+      id: 'company_name',
+      label: 'Company Name / College Name ',
+      type: 'text',
+      errorMessage: '',
       required: false,
       showError: false,
     },
     {
-      id: "designation",
-      label: "Designation ",
-      type: "text",
-      errorMessage: "",
+      id: 'designation',
+      label: 'Designation ',
+      type: 'text',
+      errorMessage: '',
       required: false,
       showError: false,
     },
     {
-      id: "github_id",
-      label: "Github_id ",
-      type: "text",
-      errorMessage: "GitHub username is required",
+      id: 'github_id',
+      label: 'Github_id ',
+      type: 'text',
+      errorMessage: 'GitHub username is required',
       required: true,
       showError: false,
     },
     {
-      id: "linkedin_id",
-      label: "linkedin_id ",
-      type: "text",
-      errorMessage: "linkedIn username is required",
+      id: 'linkedin_id',
+      label: 'linkedin_id ',
+      type: 'text',
+      errorMessage: 'linkedIn username is required',
       required: true,
       showError: false,
     },
     {
-      id: "twitter_id",
-      label: "twitter_id ",
-      type: "text",
-      errorMessage: "Twitter handle is required",
+      id: 'twitter_id',
+      label: 'twitter_id ',
+      type: 'text',
+      errorMessage: 'Twitter handle is required',
       required: true,
       showError: false,
     },
     {
-      id: "website",
-      label: "Website ",
-      type: "text",
-      errorMessage: "",
+      id: 'website',
+      label: 'Website ',
+      type: 'text',
+      errorMessage: '',
       required: false,
       showError: false,
     },
@@ -144,13 +144,13 @@ export default class SignupController extends Controller {
 
     if (this.fields[index].required && !value) {
       this.isSubmitDisabled = true;
-      set(this.fields[index], "showError", true);
+      set(this.fields[index], 'showError', true);
     } else if (!this.fields[index].validator) {
-      set(this.fields[index], "showError", false);
+      set(this.fields[index], 'showError', false);
     }
 
     const anyErrors = this.fields.map((field) => {
-      if (field.required && this.formData[field.id] === "") {
+      if (field.required && this.formData[field.id] === '') {
         return true;
       }
       return false;
@@ -164,30 +164,30 @@ export default class SignupController extends Controller {
   }
 
   @action phoneNumberValidator(phone_number) {
-    if (typeof phone_number !== "string") {
+    if (typeof phone_number !== 'string') {
       return false;
     }
 
     const pattern = /^(0|[+91]{3})?[7-9][0-9]{9}$/;
-    const index = this.fields.findIndex((field) => field.id === "phone_number");
+    const index = this.fields.findIndex((field) => field.id === 'phone_number');
 
     if (pattern.test(phone_number)) {
-      set(this.fields[index], "showError", false);
+      set(this.fields[index], 'showError', false);
     } else {
-      set(this.fields[index], "showError", true);
+      set(this.fields[index], 'showError', true);
     }
   }
 
   @action emailValidator(email) {
-    if (typeof email !== "string") return false;
+    if (typeof email !== 'string') return false;
 
     const pattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    const index = this.fields.findIndex((field) => field.type === "email");
+    const index = this.fields.findIndex((field) => field.type === 'email');
 
     if (pattern.test(email)) {
-      set(this.fields[index], "showError", false);
+      set(this.fields[index], 'showError', false);
     } else {
-      set(this.fields[index], "showError", true);
+      set(this.fields[index], 'showError', true);
     }
     return;
   }
@@ -198,21 +198,21 @@ export default class SignupController extends Controller {
     e.preventDefault();
     try {
       const response = await fetch(`${BASE_URL}/users/self`, {
-        method: "PATCH",
+        method: 'PATCH',
         body: JSON.stringify(this.formData),
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
-        credentials: "include",
+        credentials: 'include',
       });
       const data = await response.json();
       const { statusCode } = data;
       if (statusCode === 204) {
-        return window.open("https://www.realdevsquad.com/goto", "_self");
+        return window.open('https://www.realdevsquad.com/goto', '_self');
       }
-      alert("Something went wrong");
+      alert('Something went wrong');
     } catch (error) {
-      console.log("Error : ", error);
+      console.log('Error : ', error);
     }
   }
 }
