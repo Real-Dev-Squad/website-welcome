2e43163fde223e6b91dde94c32f1ff4569eff199
adnanraghib1308 <adnan.raghib@gmail.com>
2021-01-28 08:15:14 +0530

    Created /notification page

--- app/router.js
+++ app/router.js
@@ -8,4 +8,5 @@ export default class Router extends EmberRouter {
 
 Router.map(function() {
   this.route('signup');
+  this.route('notification')
 });
---
+++ app/routes/notification.js
@@ -0,0 +1,23 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+  model() {
+    return [{
+        title: "Hello User",
+        description: "Welcome to RDS"
+    },
+    {
+        title: "Your PR is pending",
+        description: "Your PR for my website is pending. Kindly check"
+    },
+    {
+        title: "Workshop on Dynamic Programming",
+        description: "Hey user we, are organizing a workshop on dynamic programming from next week. You will find the link soon. Kindly join."
+    },
+    {
+      title: "Merge Successful",
+      description: "Your PR is Successfully Merged. Thank you for your contribution"
+    }
+    ];
+  }
+});
