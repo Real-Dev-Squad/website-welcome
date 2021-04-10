d5c00c5ab32d2f64d572960b20202688a4423645
shetty-shithil <shithilshetty05@gmail.com>
2021-04-10 18:55:18 +0530

    User profile-pic added on welcome page

--- js/login.js
+++ js/login.js
@@ -1,19 +1,32 @@
 function setNameIfFound(name) {
-  const userLogin = document.querySelector(".login-btn");
+  const userLogin = document.querySelector('.login-btn');
+
   if (name) {
     userLogin.innerHTML = `Hello, ${name}!`;
   }
 }
 
-function fetchData() {
-  const url = "https://api.realdevsquad.com/users/self";
+function setPhoto(username) {
+  if (username) {
+    const userImgEl = document.querySelector('.user-profile-pic');
 
+    const userImgURL = `https://raw.githubusercontent.com/Real-Dev-Squad/website-static/main/members/${username}/img.png`;
+    userImgEl.src = userImgURL;
+  }
+}
+
+function fetchData() {
+  const url = 'https://api.realdevsquad.com/users/self';
   fetch(url, {
-    credentials: "include",
+    credentials: 'include',
   })
     .then((res) => res.json())
-    .then((res) => setNameIfFound(res.first_name))
+    .then((res) => {
+      setNameIfFound(res.first_name);
+      setPhoto(res.username);
+    })
+    .then((res) => console.log(res))
     .catch((err) => console.log(err));
 }
 
-window.addEventListener("DOMContentLoaded", fetchData);
+window.addEventListener('DOMContentLoaded', fetchData);
