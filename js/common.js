c734f3c6ad5b75181d57afd5dd8513e4ba06d87b
rucha.patil18@vit.edu <rucha.patil18@vit.edu>
2021-04-18 22:27:47 +0530

    Adds formatting settings to .prettierrc.json

--- js/common.js
+++ js/common.js
@@ -1,12 +1,12 @@
-const headerMenuBtn = document.querySelector(".header__menu-toggle");
-const headerMenu = document.querySelector(".header__center");
-const headerMobileToggle = document.querySelector(".header__mobile-toggle");
+const headerMenuBtn = document.querySelector('.header__menu-toggle')
+const headerMenu = document.querySelector('.header__center')
+const headerMobileToggle = document.querySelector('.header__mobile-toggle')
 const handleHeaderClick = () => {
-  headerMenu.classList.toggle("open-menu");
+  headerMenu.classList.toggle('open-menu')
   // headerMenu.classList.toggle('active');
-};
+}
 
-window.addEventListener("DOMContentLoaded", () => {
-  headerMenuBtn.addEventListener("click", handleHeaderClick);
-  headerMobileToggle.addEventListener("click", handleHeaderClick);
-});
+window.addEventListener('DOMContentLoaded', () => {
+  headerMenuBtn.addEventListener('click', handleHeaderClick)
+  headerMobileToggle.addEventListener('click', handleHeaderClick)
+})
