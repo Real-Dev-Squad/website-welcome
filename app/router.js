d5275852c18dc9c8d57b035141435dac05abf495
Swaraj <swarajrajpure@gmail.com>
2021-04-28 21:30:24 +0530

    local navbar

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function () {
+  // this.route('home', { path: '/' });
   this.route('signup');
   this.route('notifications');
   this.route('tasks');
