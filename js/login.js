286cd6bf290b83fb0317e8ef451ea68c22a7290d
rucha.patil18@vit.edu <rucha.patil18@vit.edu>
2021-05-21 09:36:01 +0530

    setting semicolon to true

--- js/login.js
+++ js/login.js
@@ -1,19 +1,19 @@
 function setNameIfFound(name) {
-  const userLogin = document.querySelector('.login-btn')
+  const userLogin = document.querySelector('.login-btn');
   if (name) {
-    userLogin.innerHTML = `Hello, ${name}!`
+    userLogin.innerHTML = `Hello, ${name}!`;
   }
 }
 
 function fetchData() {
-  const url = 'https://api.realdevsquad.com/users/self'
+  const url = 'https://api.realdevsquad.com/users/self';
 
   fetch(url, {
     credentials: 'include',
   })
     .then((res) => res.json())
     .then((res) => setNameIfFound(res.first_name))
-    .catch((err) => console.log(err))
+    .catch((err) => console.log(err));
 }
 
-window.addEventListener('DOMContentLoaded', fetchData)
+window.addEventListener('DOMContentLoaded', fetchData);
