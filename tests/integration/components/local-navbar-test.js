eb06d6f90891d11ed95a65de9856462f9b6d2968
Swaraj Rajpure <56217868+swarajpure@users.noreply.github.com>
2021-07-12 01:50:54 +0530

    Sync main with develop (#109)
    * https development using local-ssl-proxy
    
    * Install required packages
    
    * Cursor change to pointer on hover
    
    * Update README file for CORS errors
    
    * local navbar
    
    * adds underline on hover and moves component to application.hbs
    
    * test for local navbar component
    
    * adds profile to navbar and mobile responsive
    
    * home page changed, added border-bottom to navbar
    
    * fixed CSS on all pages
    
    * fixed p tag to span to maintain consistency
    
    * removes signup from navbar
    
    * removes signup from navbar
    
    Co-authored-by: Sanyogita <sanyogita.tilak18@vit.edu>
    Co-authored-by: anshika-nickelfox <anshika@nickelfox.co>
    Co-authored-by: Ankush Dharkar <ankushdharkar@users.noreply.github.com>

---
+++ tests/integration/components/local-navbar-test.js
@@ -0,0 +1,18 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | local-navbar', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+    const navbarContent = 'Home Profile Tasks Notifications';
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`<LocalNavbar />`);
+
+    assert.dom('.local-navbar').hasText(navbarContent);
+  });
+});
