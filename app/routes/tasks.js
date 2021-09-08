b62dc9a8045f9c142ad19a2a617104cee35d26e6
Himanshu Mishra <himanshukm2301@gmail.com>
2021-09-08 20:47:56 +0530

    fix; show error-message

--- app/routes/tasks.js
+++ app/routes/tasks.js
@@ -5,11 +5,20 @@ const API_BASE_URL = ENV.BASE_API_URL;
 
 export default class TasksRoute extends Route {
   model = async () => {
-    const response = await fetch(`${API_BASE_URL}/tasks/self`, {
-      credentials: 'include',
-    });
-    const allTasks = await response.json();
-
-    return allTasks;
+    try {
+      const response = await fetch(`${API_BASE_URL}/tasks/self`, {
+        credentials: 'include',
+      });
+      const allTasks = await response.json();
+      if (!response.ok) {
+        if (response.status == 401) {
+          throw new Error('Please log in to continue');
+        }
+        throw new Error('Oops, We ran into a probelm!');
+      }
+      return allTasks;
+    } catch (error) {
+      alert(error);
+    }
   };
 }
