d00f526927c37ab2c9f2c52a1beb63eed8c8196a
ghosharnab969 <ghosharnab969@gmail.com>
2021-09-20 22:33:10 +0530

    changes to my owm repo

--- js/common.js
+++ js/common.js
@@ -1,12 +1,23 @@
-const headerMenuBtn = document.querySelector('.header__menu-toggle');
-const headerMenu = document.querySelector('.header__center');
-const headerMobileToggle = document.querySelector('.header__mobile-toggle');
-const handleHeaderClick = () => {
-  headerMenu.classList.toggle('open-menu');
-  // headerMenu.classList.toggle('active');
-};
+// const headerMenuBtn = document.querySelector('.header__menu-toggle');
+// const headerMenu = document.querySelector('.header__center');
+// const headerMobileToggle = document.querySelector('.header__mobile-toggle');
+// const handleHeaderClick = () => {
+//   headerMenu.classList.toggle('open-menu');
+//   headerMenu.classList.toggle('active');
+// };
 
-window.addEventListener('DOMContentLoaded', () => {
-  headerMenuBtn.addEventListener('click', handleHeaderClick);
-  headerMobileToggle.addEventListener('click', handleHeaderClick);
-});
+// window.addEventListener('DOMContentLoaded', () => {
+//   headerMenuBtn.addEventListener('click', handleHeaderClick);
+//   headerMobileToggle.addEventListener('click', handleHeaderClick);
+// });
+
+const navSlide = () => {
+  const burger = document.querySelector('.hamburger');
+  const nav = document.querySelector('nav.main-nav');
+
+  burger.addEventListener('click', () => {
+    console.log(nav.classList);
+    nav.classList.toggle('.main-nav-active');
+  });
+};
+navSlide();
