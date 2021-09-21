a44e9b6f0209587a837c29d1d3ddc93bbcffd659
Himanshu Mishra <himanshukm2301@gmail.com>
2021-09-21 12:28:53 +0530

    Fixed- Show Error Message if API Fails

--- app/routes/tasks.js
+++ app/routes/tasks.js
@@ -9,16 +9,19 @@ export default class TasksRoute extends Route {
       const response = await fetch(`${API_BASE_URL}/tasks/self`, {
         credentials: 'include',
       });
-      const allTasks = await response.json();
       if (!response.ok) {
         if (response.status == 401) {
           throw new Error('Please log in to continue');
         }
-        throw new Error('Oops, We ran into a probelm!');
+        throw new Error('Oops, We ran into a problem!');
       }
-      return allTasks;
+      return await response.json();
     } catch (error) {
       alert(error);
+      window.open(
+        'https://github.com/login?client_id=23c78f66ab7964e5ef97&return_to=%2Flogin%2Foauth%2Fauthorize%3Fclient_id%3D23c78f66ab7964e5ef97',
+        '_self'
+      );
     }
   };
 }
