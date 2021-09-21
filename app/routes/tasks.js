c905f1981e154e1a2f2af0776d35089b84412d14
Himanshu Mishra <himanshukm2301@gmail.com>
2021-09-21 23:57:20 +0530

    Fixed- Show Error Message if API Fails

--- app/routes/tasks.js
+++ app/routes/tasks.js
@@ -19,7 +19,7 @@ export default class TasksRoute extends Route {
     } catch (error) {
       alert(error);
       window.open(
-        'https://github.com/login?client_id=23c78f66ab7964e5ef97&return_to=%2Flogin%2Foauth%2Fauthorize%3Fclient_id%3D23c78f66ab7964e5ef97',
+        'https://github.com/login/oauth/authorize?client_id=23c78f66ab7964e5ef97',
         '_self'
       );
     }
