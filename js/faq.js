6555a8c516f916f3163b3fa522548f30ae014435
Manie-oss <to.mansip@gmail.com>
2021-10-01 12:03:41 +0530

    added a link for a particular FAQ

--- js/faq.js
+++ js/faq.js
@@ -1,4 +1,19 @@
 const faqTitles = document.querySelectorAll('.faq__title');
+const faqLinks = document.querySelectorAll('.user_FAQ');
+
+var currentLocation = window.location.hash;
+
+faqLinks.forEach((faqLink) => {
+  const faqLinkValue = faqLink.hash;
+  if(currentLocation === faqLinkValue){
+    const anctag = document.querySelector(`a[href="${faqLinkValue}"]`);
+    const faqNode = anctag.parentNode.parentNode;
+    faqNode.classList.toggle('show');
+    const faqText = faqNode.nextElementSibling;
+    faqNode.childNodes[3].innerHTML = '-';
+    faqText.style.maxHeight = faqText.scrollHeight + 'px';
+  }
+});
 
 faqTitles.forEach((faqTitle) => {
   faqTitle.addEventListener('click', (event) => {
