26c26015485faa8e9bfd37758400cd793547ca45
Manie-oss <to.mansip@gmail.com>
2021-10-06 16:40:08 +0530

    modified js code

--- js/faq.js
+++ js/faq.js
@@ -1,31 +1,6 @@
 const faqTitles = document.querySelectorAll('.faq__title');
 const faqLinks = document.querySelectorAll('.faq_link');
-
 const currentLocation = window.location.hash;
-
-function showAnswer(faqTitle) {
-  faqTitle.classList.toggle('show');
-  var faqText = faqTitle.nextElementSibling;
-  if (faqTitle.classList.contains('show')) {
-    faqTitles.forEach((faqTitle) => {
-      faqTitle.childNodes[3].innerHTML = '+';
-    });
-    faqTitle.childNodes[3].innerHTML = '-';
-    faqText.style.maxHeight = faqText.scrollHeight + 'px';
-  } else {
-    faqText.style.maxHeight = 0;
-    faqTitle.childNodes[3].innerHTML = '+';
-  }
-}
-
-faqLinks.forEach((faqLink) => {
-  const faqLinkValue = faqLink.hash;
-  if (currentLocation === faqLinkValue) {
-    const ancTag = document.querySelector(`a[href="${faqLinkValue}"]`);
-    const faqTitle = ancTag.parentNode.parentNode;
-    showAnswer(faqTitle);
-  }
-});
 
 faqTitles.forEach((faqTitle) => {
   faqTitle.addEventListener('click', (event) => {
@@ -34,6 +9,28 @@ faqTitles.forEach((faqTitle) => {
       currentlyActiveFaqTitle.classList.toggle('show');
       currentlyActiveFaqTitle.nextElementSibling.style.maxHeight = 0;
     }
-    showAnswer(faqTitle);
+
+    faqTitle.classList.toggle('show');
+    const faqNode = faqTitle.parentNode;
+    const faqText = faqNode.nextElementSibling;
+    if (faqTitle.classList.contains('show')) {
+      faqTitles.forEach((faqTitle) => {
+        faqTitle.childNodes[3].innerHTML = '+';
+      });
+      faqTitle.childNodes[3].innerHTML = '-';
+      faqText.style.maxHeight = faqText.scrollHeight + 'px';
+    } else {
+      faqText.style.maxHeight = 0;
+      faqTitle.childNodes[3].innerHTML = '+';
+    }
   });
+});
+
+faqLinks.forEach((faqLink) => {
+  const faqLinkValue = faqLink.hash;
+  if (currentLocation === faqLinkValue) {
+    const ancTag = document.querySelector(`a[href="${faqLinkValue}"]`);
+    const faqTitle = ancTag.firstElementChild;
+    faqTitle.click();
+  }
 });
