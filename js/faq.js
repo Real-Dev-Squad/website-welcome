e08a95df8e727741244476c2ae25ee571b5fbd7f
Manie-oss <to.mansip@gmail.com>
2021-10-14 18:19:21 +0530

    removed the inner loop which was used to handle the button inner content

--- js/faq.js
+++ js/faq.js
@@ -4,24 +4,30 @@ const currentLocation = window.location.hash;
 faqLinks.forEach((faqLink) => {
   faqLink.addEventListener('click', (event) => {
     const currentlyActiveFaqLink = document.querySelector('.faq_link.show');
+    const currentlyActiveFaqButton = document.querySelector('.faq-btn.show');
+
     if (currentlyActiveFaqLink && currentlyActiveFaqLink !== faqLink) {
       currentlyActiveFaqLink.classList.toggle('show');
+      currentlyActiveFaqButton.classList.toggle('show');
       currentlyActiveFaqLink.nextElementSibling.style.maxHeight = 0;
     }
+
     faqLink.classList.toggle('show');
+    faqLink.firstElementChild.lastElementChild.classList.toggle('show');
     const faqText = faqLink.nextElementSibling;
-    if (faqLink.classList.contains('show')) {
-      faqLinks.forEach((faqLink) => {
-        faqLink.firstElementChild.lastElementChild.innerHTML = '+';
-      });
-      faqLink.firstElementChild.lastElementChild.innerHTML = '-';
+
+    if (
+      faqLink.classList.contains('show') &&
+      faqLink.firstElementChild.lastElementChild.classList.contains('show')
+    ) {
       faqText.style.maxHeight = faqText.scrollHeight + 'px';
     } else {
       faqText.style.maxHeight = 0;
-      faqLink.firstElementChild.lastElementChild.innerHTML = '+';
     }
   });
+
   const faqLinkValue = faqLink.hash;
+
   if (currentLocation === faqLinkValue) {
     const ancTag = document.querySelector(`a[href="${faqLinkValue}"]`);
     ancTag.click();
