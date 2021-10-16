872f69560e802045a84c05b76b8ce4f3580a1416
Mansi Patel <53491434+Manie-oss@users.noreply.github.com>
2021-10-16 16:15:27 +0530

    add link for a particular FAQ (#188)
    * added a link for a particular FAQ
    
    * removed inline style to class
    
    * renamed the class
    
    * renamed the class
    
    * renamed the class
    
    * formatted with prettier
    
    * simplified js code
    
    * modified js code
    
    * simplified js code
    
    * removed unnecessary console logs
    
    * modified js code
    
    * modified js code
    
    * button content is made empty and set using css
    
    * removed the inner loop which was used to handle the button inner content
    
    * added pseudo selectors on button
    
    * removed the unused id
    
    * added the vairable for expand button icon

--- js/faq.js
+++ js/faq.js
@@ -1,24 +1,37 @@
-const faqTitles = document.querySelectorAll('.faq__title');
+const faqLinks = document.querySelectorAll('.faq_link');
+const currentLocation = window.location.hash;
 
-faqTitles.forEach((faqTitle) => {
-  faqTitle.addEventListener('click', (event) => {
-    const currentlyActiveFaqTitle = document.querySelector('.faq__title.show');
-    if (currentlyActiveFaqTitle && currentlyActiveFaqTitle !== faqTitle) {
-      currentlyActiveFaqTitle.classList.toggle('show');
-      currentlyActiveFaqTitle.nextElementSibling.style.maxHeight = 0;
+faqLinks.forEach((faqLink) => {
+  faqLink.addEventListener('click', (event) => {
+    const currentlyActiveFaqLink = document.querySelector('.faq_link.show');
+    const currentlyActiveFaqButton = document.querySelector('.faq-btn.show');
+
+    if (currentlyActiveFaqLink && currentlyActiveFaqLink !== faqLink) {
+      currentlyActiveFaqLink.classList.toggle('show');
+      currentlyActiveFaqButton.classList.toggle('show');
+      currentlyActiveFaqLink.nextElementSibling.style.maxHeight = 0;
     }
 
-    faqTitle.classList.toggle('show');
-    const faqText = faqTitle.nextElementSibling;
-    if (faqTitle.classList.contains('show')) {
-      faqTitles.forEach((faqTitle) => {
-        faqTitle.childNodes[3].innerHTML = '+';
-      });
-      faqTitle.childNodes[3].innerHTML = '-';
+    const faqExpandIcon = faqLink.firstElementChild.lastElementChild;
+
+    faqLink.classList.toggle('show');
+    faqExpandIcon.classList.toggle('show');
+    const faqText = faqLink.nextElementSibling;
+
+    if (
+      faqLink.classList.contains('show') &&
+      faqExpandIcon.classList.contains('show')
+    ) {
       faqText.style.maxHeight = faqText.scrollHeight + 'px';
     } else {
       faqText.style.maxHeight = 0;
-      faqTitle.childNodes[3].innerHTML = '+';
     }
   });
+
+  const faqLinkValue = faqLink.hash;
+
+  if (currentLocation === faqLinkValue) {
+    const ancTag = document.querySelector(`a[href="${faqLinkValue}"]`);
+    ancTag.click();
+  }
 });
