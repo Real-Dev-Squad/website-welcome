cc2eeca37c727eeab2f60510de9d3f4f96ff2fa6
dhanush027 <79807326+dhanush027@users.noreply.github.com>
2022-01-12 20:55:57 +0530

    Updated Navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,15 +1,14 @@
-window.addEventListener('DOMContentLoaded',(event)=>{
+window.addEventListener('DOMContentLoaded', (event) => {
   const checkURL = window.location.href;
-  const stagingURL = "https://staging-welcome.realdevsquad.com/";
   const productionURL = "https://welcome.realdevsquad.com/";
+  let userLogin = document.getElementById('user-login');
 
-  if (checkURL == stagingURL) {
-    document.getElementById("user-login").href = "https://github.com/login/oauth/authorize?client_id=c4a84431feaf604e89d1";
+  if (checkURL === productionURL) {
+    userLogin.href = "https://github.com/login/oauth/authorize?client_id=23c78f66ab7964e5ef97";
    }
   else {
-    document.getElementById("user-login").href = "https://github.com/login/oauth/authorize?client_id=23c78f66ab7964e5ef97";
+    userLogin.href = "https://github.com/login/oauth/authorize?client_id=c4a84431feaf604e89d1";
   }
-
 })
 
 const hamburger = document.querySelector('.hamburger');
