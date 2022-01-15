a20a7c5397de3253036e1e85ada9d69eb1c08fe0
dhanush027 <dhanushkumarnaidu27@gmail.com>
2022-01-15 14:51:12 +0530

    Updates clientID for user,based on the environment

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,13 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  const checkURL = window.location.href;
+  const productionURL = 'https://welcome.realdevsquad.com/';
+  let userLoginEl = document.getElementById('user-login');
+  const clientId =
+    checkURL === productionURL
+      ? '23c78f66ab7964e5ef97'
+      : 'c4a84431feaf604e89d1';
+  userLoginEl.href = `https://github.com/login/oauth/authorize?client_id=${clientId}`;
+});
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 
