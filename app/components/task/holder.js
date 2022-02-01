2cad7eb9e0660352a977432ebf113f398c9df96d
Swaraj <swarajrajpure@gmail.com>
2022-02-02 00:26:57 +0530

    feat: change task statuses to new ones

--- app/components/task/holder.js
+++ app/components/task/holder.js
@@ -1,8 +1,8 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
-import TASK_STATUS from 'website-my/constants/tasks';
+import { STATUS, TASK_STATUSES } from 'website-my/constants/tasks';
 
-const { ACTIVE, BLOCKED, COMPLETED, PENDING } = TASK_STATUS;
+const { ACTIVE, BLOCKED, COMPLETED, PENDING } = STATUS;
 
 export default class TasksHolderComponent extends Component {
   @action
@@ -16,6 +16,12 @@ export default class TasksHolderComponent extends Component {
     const { value } = e.target;
     this.args.onTaskChange('status', value);
   }
+
+  @action handleTaskStatusChange(e) {
+    console.log(e.target.value);
+  }
+
+  taskStatus = TASK_STATUSES;
 
   availableStatusOptions = [ACTIVE, BLOCKED, COMPLETED, PENDING];
 }
--- app/constants/tasks.js
+++ app/constants/tasks.js
@@ -5,4 +5,63 @@ const STATUS = {
   PENDING: 'pending',
 };
 
-export default STATUS;
+const TASK_STATUSES = [
+  {
+    label: 'Available',
+    key: 'AVAILABLE',
+  },
+  {
+    label: 'Assigned',
+    key: 'ASSIGNED',
+  },
+  {
+    label: 'In Progress',
+    key: 'IN_PROGRESS',
+  },
+  {
+    label: 'Blocked',
+    key: 'BLOCKED',
+  },
+  {
+    label: 'Smoke Testing',
+    key: 'SMOKE_TESING',
+  },
+  {
+    label: 'Completed',
+    key: 'COMPLETED',
+  },
+  {
+    label: 'Needs Review',
+    key: 'NEEDS_REVIEW',
+  },
+  {
+    label: 'In Review',
+    key: 'IN_REVIEW',
+  },
+  {
+    label: 'Approved',
+    key: 'APPROVED',
+  },
+  {
+    label: 'Merged',
+    key: 'MERGED',
+  },
+  {
+    label: 'Sanity Check',
+    key: 'SANITY_CHECK',
+  },
+  {
+    label: 'Regression Check',
+    key: 'REGRESSION_CHECK',
+  },
+  {
+    label: 'Released',
+    key: 'RELEASED',
+  },
+  {
+    label: 'Verified',
+    key: 'VERIFIED',
+  },
+];
+
+export { STATUS, TASK_STATUSES };
