40b582a0e5841b6eac4d593a0a9f7b0db98969e2
aakankshadhurandhar34 <aakanksha.dhurandhar@oslash.com>
2022-04-02 21:48:43 +0530

    feature: dark mode for welcome site

---
+++ js/darkmode.js
@@ -0,0 +1,43 @@
+const icon = document.getElementById('icon');
+onload = onload();
+icon.onclick = function () {
+  const element = document.body;
+  element.classList.toggle('dark-theme');
+  if (element.classList.contains('dark-theme')) {
+    icon.src = 'img/icons/sun.png';
+    setCookie('theme', 'dark', 30);
+  } else {
+    icon.src = 'img/icons/moon.png';
+    setCookie('theme', 'light', 30);
+  }
+};
+
+function onload() {
+  if (getCookie('theme') === 'light') {
+    document.body.classList.remove('dark-theme');
+    icon.src = 'img/icons/moon.png';
+  } else {
+    document.body.classList.add('dark-theme');
+    icon.src = 'img/icons/sun.png';
+  }
+}
+
+function setCookie(name, value, days = 30) {
+  const domain = '.realdevsquad.com';
+  const date = new Date();
+  date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+  const expires = '; expires=' + date.toUTCString();
+  document.cookie = `${name}=${value}; expires=${expires}; domain=${domain}; path=/`;
+}
+
+function getCookie(name) {
+  const cookieName = name + '=';
+  const splitCookie = document.cookie.split(';');
+  for (let i = 0; i < splitCookie.length; i++) {
+    var c = splitCookie[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(cookieName) == 0)
+      return c.substring(cookieName.length, c.length);
+  }
+  return null;
+}
