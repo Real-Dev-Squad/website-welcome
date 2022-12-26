54d58150c92724cdfe65e2464c769bf745ac3b72
X vamp <vinit>
2022-12-26 20:35:58 +0530

    add tab indexing

--- js/faq.js
+++ js/faq.js
@@ -2,27 +2,41 @@ const faqLinks = document.querySelectorAll('.faq_link');
 const currentLocation = window.location.hash;
 
 faqLinks.forEach((faqLink) => {
+
+  const faqText = faqLink.nextElementSibling;
+  const faqExpandIcon = faqLink.firstElementChild.lastElementChild;
+
   faqLink.addEventListener('click', (event) => {
+
+    const tabIndexing = faqLink.nextElementSibling.querySelectorAll('a')
+    //  console.log(tabIndexing);
     const currentlyActiveFaqLink = document.querySelector('.faq_link.show');
     const currentlyActiveFaqButton = document.querySelector('.faq-btn.show');
+
 
     if (currentlyActiveFaqLink && currentlyActiveFaqLink !== faqLink) {
       currentlyActiveFaqLink.classList.toggle('show');
       currentlyActiveFaqButton.classList.toggle('show');
       currentlyActiveFaqLink.nextElementSibling.style.maxHeight = 0;
     }
-
-    const faqExpandIcon = faqLink.firstElementChild.lastElementChild;
-
+  
     faqLink.classList.toggle('show');
-    faqExpandIcon.classList.toggle('show');
-    const faqText = faqLink.nextElementSibling;
-
+    faqExpandIcon.classList.toggle('show'); 
+    tabIndexing.forEach((element)=>{
+      const previousTabIndex = element.getAttribute("tabindex")
+      // console.log(previousTabIndex);
+      element.setAttribute("tabindex", previousTabIndex=="1"?"-1":"1")
+    })
+    
     if (
       faqLink.classList.contains('show') &&
       faqExpandIcon.classList.contains('show')
     ) {
       faqText.style.maxHeight = faqText.scrollHeight + 'px';
+      // faqLists.forEach((element)=>{
+      //   element.setAttribute("tabindex","1")
+      // })
+
     } else {
       faqText.style.maxHeight = 0;
     }
@@ -34,4 +48,20 @@ faqLinks.forEach((faqLink) => {
     const ancTag = document.querySelector(`a[href="${faqLinkValue}"]`);
     ancTag.click();
   }
+  //   faqLink.addEventListener("keyup", (e)=>{
+  //   // console.log("space pressed", e.code);
+  //   if(e.code==="Space"){
+  //     faqLink.classList.add('show');
+  //     faqText.style.maxHeight = faqText.scrollHeight + 'px';
+      
+  //     removeEvent();
+  //     // faqLink.classList.remove('show');
+  //   }
+  //   function removeEvent(){
+  //     faqLink.removeEventListener('keyup', e);
+  //     faqText.style.maxHeight = 0 + 'px';
+  //     // e.preventDefault();
+  //     faqLink.classList.remove('show');
+  //   }
+  // })
 });
