2e08dcf575d652a7813713aecb0f2db73bbaffc4
SahsiSunny <sahsisunny@gmail.com>
2023-01-19 11:43:45 +0530

    fix dropdown menu issue and position

--- js/navbar.js
+++ js/navbar.js
@@ -18,3 +18,38 @@ hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
 });
+
+const signout = () => {
+  fetch('https://api.realdevsquad.com/auth/signout', {
+    method: 'GET',
+    credentials: 'include',
+  }).then(() => {
+    location.reload();
+  });
+};
+
+document.getElementById('signout-option').addEventListener('click', () => {
+  signout();
+});
+
+const myProfileButton = document.getElementById('my-profile');
+myProfileButton.addEventListener('click', () => {
+  window.location.href = 'https://my.realdevsquad.com/';
+});
+
+document.querySelectorAll('.user-greet').forEach((greet) => {
+  greet.addEventListener('click', () => {
+    document.querySelector('.dropdown').classList.toggle('hide');
+  });
+});
+
+const dropdownTrigger = document.querySelectorAll('.user-greet');
+document.addEventListener('click', (event) => {
+  if (
+    dropdownTrigger[0] != event.target.parentElement &&
+    dropdownTrigger[1] != event.target.parentElement &&
+    !document.querySelector('.dropdown').classList.contains('hide')
+  ) {
+    document.querySelector('.dropdown').classList.add('hide');
+  }
+});
